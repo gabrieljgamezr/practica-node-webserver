5e4f9ebd49e3cb56491f66ca22647d1f7c270c8e
Gabriel Gamez <gabrieljgamezr@gmail.com>
2023-01-14 07:08:58 -0400

    Webserver listo

---
+++ app-old.js
@@ -0,0 +1,16 @@
+
+const http = require('http');
+
+http.createServer( (req, res) => {
+
+    // res.writeHead(200, {'Content-Type': 'application/json'});
+    // res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    // res.writeHead(200, {'Content-Type': 'application/csv'});
+
+    res.write('Hola Mundo');
+
+    res.end();
+
+}).listen(8080);
+
+console.log('Escuchando en el puerto', 8080);
