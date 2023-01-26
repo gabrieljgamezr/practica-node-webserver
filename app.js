316674380c739c87e9b191a154dcc9c2b521c9a3
Gabriel Gamez <gabrieljgamezr@gmail.com>
2023-01-26 19:05:39 -0400

    Comentario añadidos

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+// Para crear y configurar variables de entorno.
 require ('dotenv').config();
+// Exportar express para crear el webserver.
 const express = require('express');
+// Importar HBS para crear las vistas.
 const hbs = require('hbs');
 
+// Iniciar express
 const app = express();
-const port = process.env.PORT;
+// Definir puerto y en caso de ni existir un puerto definido, utilizará el puerto 3000.
+const port = process.env.PORT || 3000;
 
 //Handlebars
 app.set('view engine', 'hbs');
@@ -12,6 +17,7 @@ hbs.registerPartials(__dirname + '/views/partials');
 // Servir contenido estático
 app.use(express.static('public'));
 
+// Manejar las rutas
 app.get('/', (req, res) => {
     res.render('home', {
         nombre: 'Gabriel Gamez',
@@ -33,10 +39,12 @@ app.get('/elements', (req, res) => {
     });
 });
 
+// En caso de entrar en una ruta no específicada, rediríjira a un mensaje de error.
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html');
 });
 
+// Listen que muestra un mensaje en consola de que el servidor se está ejecutando y en qué puerto
 app.listen(port, () => {
     console.log(`Aplicación de ejemplo escuchada en http://localhost:${port}`);
 });
